c5c648606922caa1f386b82eae25663aba0d25b7
shiavaregalla99 <shivaregalla99@gmail.com>
2023-03-30 08:59:57 +0530

    class1 class2 class3

---
+++ src/Class2/DataTypes.java
@@ -0,0 +1,24 @@
+package Class2;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        byte value1=100;
+        short value2=300;
+        int value3=500;
+        long value4=89898532453l;
+        float value5=787.009f;
+        double value6=8989.0909;
+        char value7='5';
+        boolean value8=true;
+
+        //int -----> Data type
+        //value1 ------> referance variable you can give any menaing ful name
+        // = ---------> assignment operator to assign the value you are using it
+        //500 ----> integer value
+        //; --------> termination statement
+
+
+
+    }
+}
