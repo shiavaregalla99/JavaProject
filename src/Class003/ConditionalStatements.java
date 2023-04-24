43e037d6b7426db5b5e1862f56f19232cbc966ad
shiavaregalla99 <shivaregalla99@gmail.com>
2023-04-24 11:40:55 +0530

    commit 3

--- src/Class003/ConditionalStatements.java
+++ src/Class003/ConditionalStatements.java
@@ -1,4 +1,10 @@
 package Class003;
 
 public class ConditionalStatements {
+    public static void main(String[] args) {
+      double num=10.000989878786;
+        System.out.println(num);
+      int value=(int)num;
+        System.out.println(value);
+    }
 }
